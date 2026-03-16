b841b534a3ec514d4fd7fcccbe26233a1bf29ed8
Developers14-fred <alfredomufano14@gmail.com>
2026-03-16 14:50:23 +0200

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,11 @@
+# Store the room cost
+room_cost = 240000
+
+# Store the number of students
+number_of_students = 3
+
+# Calculate how much each student should pay
+amount_per_student = room_cost / number_of_students
+
+# Display the result
+print("Each student should pay:", amount_per_student, "FRW per semester")
